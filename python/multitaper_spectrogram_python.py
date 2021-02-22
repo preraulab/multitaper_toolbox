be9ce06a461e8a5dd92df0c0612c7e1a6880dd1b
Thomas Possidente <tpossidente@bwh.harvard.edu>
2021-02-22 11:56:25 -0500

    updated python mt to use taper weighting

--- python/multitaper_spectrogram_python.py
+++ python/multitaper_spectrogram_python.py
@@ -15,33 +15,46 @@ import librosa.display
 
 # MULTITAPER SPECTROGRAM #
 def multitaper_spectrogram(data, fs, frequency_range=None, time_bandwidth=5, num_tapers=None, window_params=None,
-                           min_nfft=0, detrend_opt='linear', multiprocess=False, cpus=False, plot_on=True, verbose=True):
+                           min_nfft=0, detrend_opt='linear', multiprocess=False, cpus=False, weighting='unity',
+                           plot_on=True, verbose=True, xyflip=False):
     """ Compute multitaper spectrogram of timeseries data
     Results tend to agree with Prerau Lab Matlab implementation of multitaper spectrogram with precision on the order
     of at most 10^-12 with SD of at most 10^-10
-            Arguments:
-                    data (1d np.array): time series data -- required
-                    fs (float): sampling frequency in Hz  -- required
-                    frequency_range (list): 1x2 list - [<min frequency>, <max frequency>] (default: [0 nyquist])
-                    time_bandwidth (float): time-half bandwidth product (window duration*half bandwidth of main lobe)
-                                            (default: 5 Hz*s)
-                    num_tapers (int): number of DPSS tapers to use (default: [will be computed
-                                      as floor(2*time_bandwidth - 1)])
-                    window_params (list): 1x2 list - [window size (seconds), step size (seconds)] (default: [5 1])
-                    detrend_opt (string): detrend data window ('linear' (default), 'constant', 'off')
-                                          (Default: 'linear')
-                    min_nfft (int): minimum allowable NFFT size, adds zero padding for interpolation (closest 2^x)
-                                    (default: 0)
-                    multiprocess (bool): Use multiprocessing to compute multitaper spectrogram (default: False)
-                    cpus (int): Number of cpus to use if multiprocess = True (default: False). Note: if default is left
-                                as False and multiprocess = True, the number of cpus used for multiprocessing will be
-                                all available - 1.
-                    plot_on (bool): plot results (default: True)
-                    verbose (bool): display spectrogram properties (default: true)
-            Returns:
-                    mt_spectrogram (TxF np array): spectral power matrix
-                    stimes (1xT np array): timepoints (s) in mt_spectrogram
-                    sfreqs (1xF np array)L frequency values (Hz) in mt_spectrogram
+
+    Usage:
+    mt_spectrogram, stimes, sfreqs = multitaper_spectrogram(data, fs, frequency_range=None, time_bandwidth=5,
+                                                                   num_tapers=None, window_params=None, min_nfft=0,
+                                                                   detrend_opt='linear', multiprocess=False, cpus=False,
+                                                                    weighting='unity', plot_on=True, verbose=True,
+                                                                    xyflip=False):
+
+        Arguments:
+                data (1d np.array): time series data -- required
+                fs (float): sampling frequency in Hz  -- required
+                frequency_range (list): 1x2 list - [<min frequency>, <max frequency>] (default: [0 nyquist])
+                time_bandwidth (float): time-half bandwidth product (window duration*half bandwidth of main lobe)
+                                        (default: 5 Hz*s)
+                num_tapers (int): number of DPSS tapers to use (default: [will be computed
+                                  as floor(2*time_bandwidth - 1)])
+                window_params (list): 1x2 list - [window size (seconds), step size (seconds)] (default: [5 1])
+                detrend_opt (string): detrend data window ('linear' (default), 'constant', 'off')
+                                      (Default: 'linear')
+                min_nfft (int): minimum allowable NFFT size, adds zero padding for interpolation (closest 2^x)
+                                (default: 0)
+                multiprocess (bool): Use multiprocessing to compute multitaper spectrogram (default: False)
+                cpus (int): Number of cpus to use if multiprocess = True (default: False). Note: if default is left
+                            as False and multiprocess = True, the number of cpus used for multiprocessing will be
+                            all available - 1.
+                weighting (str): weighting of tapers ('unity' (default), 'eigen', 'adapt');
+                plot_on (bool): plot results (default: True)
+                verbose (bool): display spectrogram properties (default: true)
+                xyflip (bool): transpose the mt_spectrogram output (default: false)
+        Returns:
+                mt_spectrogram (TxF np array): spectral power matrix
+                stimes (1xT np array): timepoints (s) in mt_spectrogram
+                sfreqs (1xF np array)L frequency values (Hz) in mt_spectrogram
+
+        Last modified - 2/18/2021 Tom P.
     """
 
     #  Process user input
@@ -69,18 +82,29 @@ def multitaper_spectrogram(data, fs, frequency_range=None, time_bandwidth=5, num
     #     STEP 4: Take the mean of the tapered spectra
 
     # Compute DPSS tapers (STEP 1)
-    DPSS_tapers = dpss(winsize_samples, time_bandwidth, num_tapers) * math.sqrt(fs)
+    dpss_tapers, dpss_eigen = dpss(winsize_samples, time_bandwidth, num_tapers, return_ratios=True)
+    dpss_eigen = np.reshape(dpss_eigen, (num_tapers, 1))
+
+    # pre-compute weights
+    if weighting == 'eigen':
+        wt = dpss_eigen / num_tapers
+    elif weighting == 'unity':
+        wt = np.ones(num_tapers) / num_tapers
+        wt = np.reshape(wt, (num_tapers, 1))  # reshape as column vector
+    else:
+        wt = 0
 
     tic = timeit.default_timer()  # start timer
 
     # set all but 1 arg of calc_mts_segment to constant (so we only have to supply one argument later)
-    calc_mts_segment_plus_args = partial(calc_mts_segment, DPSS_tapers=DPSS_tapers, nfft=nfft,
-                                         freq_inds=freq_inds, detrend_opt=detrend_opt)
+    calc_mts_segment_plus_args = partial(calc_mts_segment, dpss_tapers=dpss_tapers, nfft=nfft, freq_inds=freq_inds,
+                                         detrend_opt=detrend_opt, num_tapers=num_tapers, dpss_eigen=dpss_eigen,
+                                         weighting=weighting, wt=wt)
 
-    if multiprocess: # use multiprocessing
-        if not cpus: # if cpus not specfied, use all but 1
+    if multiprocess:  # use multiprocessing
+        if not cpus:  # if cpus not specfied, use all but 1
             pool = Pool(cpu_count()-1)
-        else: # else us specified number
+        else:  # else us specified number
             pool = Pool(cpus)
 
         # Compute multiprocess multitaper spect.
@@ -88,12 +112,22 @@ def multitaper_spectrogram(data, fs, frequency_range=None, time_bandwidth=5, num
         pool.close()
         pool.join()
 
-    else: # if no multiprocessing, compute normally
+    else:  # if no multiprocessing, compute normally
         mt_spectrogram = np.apply_along_axis(calc_mts_segment_plus_args, 1, data_segments)
 
-    # Compute mean fft magnitude (STEP 4.2)
+    # Compute one-sided PSD spectrum
     mt_spectrogram = np.asarray(mt_spectrogram)
-    mt_spectrogram = mt_spectrogram.conj().T / fs ** 2 / num_tapers
+    mt_spectrogram = mt_spectrogram.T
+    dc_select = np.where(sfreqs == 0)
+    nyquist_select = np.where(sfreqs == fs/2)
+    select = np.setdiff1d(np.arange(0, len(sfreqs)), [dc_select, nyquist_select])
+
+    mt_spectrogram = np.vstack([mt_spectrogram[dc_select[0], :], 2*mt_spectrogram[select, :],
+                               mt_spectrogram[nyquist_select[0], :]]) / fs
+
+    # Flip if requested
+    if xyflip:
+        mt_spectrogram = np.transpose(mt_spectrogram)
 
     # End timer and get elapsed compute time
     toc = timeit.default_timer()
@@ -103,17 +137,16 @@ def multitaper_spectrogram(data, fs, frequency_range=None, time_bandwidth=5, num
 
     # Plot multitaper spectrogram
     if plot_on:
+
         plt.figure(1, figsize=(10, 5))
         librosa.display.specshow(nanpow2db(mt_spectrogram), x_axis='time', y_axis='linear',
-                                 x_coords=stimes, y_coords=sfreqs, shading='auto',
-                                 cmap="jet")
+                                 x_coords=stimes, y_coords=sfreqs, shading='auto', cmap="jet")
         plt.colorbar(label='Power (dB)')
         plt.xlabel("Time (HH:MM:SS)")
         plt.ylabel("Frequency (Hz)")
         plt.show()
 
     # Put outputs into better format for output
-    mt_spectrogram = mt_spectrogram.T
     stimes = np.mat(stimes)
     sfreqs = np.mat(sfreqs)
 
@@ -264,16 +297,15 @@ def process_spectrogram_params(fs, nfft, frequency_range, window_start, datawin_
 
     # create frequency vector
     df = fs / nfft
-    sfreqs = np.arange(df/2, fs, df)
+    sfreqs = np.arange(0, fs, df)
 
     # Get frequencies for given frequency range
     freq_inds = (sfreqs >= frequency_range[0]) & (sfreqs <= frequency_range[1])
     sfreqs = sfreqs[freq_inds]
 
     # Compute times in the middle of each spectrum
-    window_middle_times = window_start + round(
-        datawin_size / 2)
-    stimes = window_middle_times / fs
+    window_middle_samples = window_start + round(datawin_size / 2)
+    stimes = window_middle_samples / fs
 
     # Get indexes for each window
     window_idxs = np.atleast_2d(window_start).T + np.arange(0, datawin_size, 1)
@@ -334,16 +366,20 @@ def nanpow2db(y):
 
 
 # CALCULATE MULTITAPER SPECTRUM ON SINGLE SEGMENT
-def calc_mts_segment(data_segment, DPSS_tapers, nfft, freq_inds, detrend_opt):
+def calc_mts_segment(data_segment, dpss_tapers, nfft, freq_inds, detrend_opt, num_tapers, dpss_eigen, weighting, wt):
     """ Helper function to calculate the multitaper spectrum of a single segment of data
         Arguments:
             data_segment (1d np.array): One window worth of time-series data -- required
-            DPSS_tapers (2d np.array): Parameters for the DPSS tapers to be used.
+            dpss_tapers (2d np.array): Parameters for the DPSS tapers to be used.
                                        Dimensions are (num_tapers, winsize_samples) -- required
             nfft (int): length of signal to calculate fft on -- required
             freq_inds (1d np array): boolean array of which frequencies are being analyzed in
                                       an array of frequencies from 0 to fs with steps of fs/nfft
             detrend_opt (str): detrend data window ('linear' (default), 'constant', 'off')
+            num_tapers (int): number of tapers being used
+            dpss_eigen (np array):
+            weighting (str):
+            wt (int or np array):
         Returns:
             mt_spectrum (1d np.array): spectral power for single window
     """
@@ -359,14 +395,33 @@ def calc_mts_segment(data_segment, DPSS_tapers, nfft, freq_inds, detrend_opt):
         data_segment = detrend(data_segment, type=detrend_opt)
 
     # Multiply data by dpss tapers (STEP 2)
-    tapered_data = np.multiply(np.mat(data_segment).T, np.mat(DPSS_tapers.T))
+    tapered_data = np.multiply(np.mat(data_segment).T, np.mat(dpss_tapers.T))
 
     # Compute the FFT (STEP 3)
     fft_data = np.fft.fft(tapered_data, nfft, axis=0)
-    fft_range = fft_data[freq_inds, :]
 
-    # Take the FFT magnitude (STEP 4.1)
-    magnitude = np.power(np.imag(fft_range), 2) + np.power(np.real(fft_range), 2)
-    mt_spectrum = np.sum(magnitude, axis=1)
+    # Compute the weighted mean spectral power across tapers (STEP 4)
+    spower = np.power(np.imag(fft_data), 2) + np.power(np.real(fft_data), 2)
+    if weighting == 'adapt':
+        # adaptive weights - for colored noise spectrum (Percival & Walden p368-370)
+        tpower = np.dot(np.transpose(data_segment), (data_segment/len(data_segment)))
+        spower_iter = np.mean(spower[:, 0:2], 1)
+        spower_iter = spower_iter[:, np.newaxis]
+        a = (1 - dpss_eigen) * tpower
+        for i in range(3):  # 3 iterations only
+            # Calc the MSE weights
+            b = np.dot(spower_iter, np.ones((1, num_tapers))) / ((np.dot(spower_iter, np.transpose(dpss_eigen))) +
+                                                                 (np.ones((nfft, 1)) * np.transpose(a)))
+            # Calc new spectral estimate
+            wk = (b**2) * np.dot(np.ones((nfft, 1)), np.transpose(dpss_eigen))
+            spower_iter = np.sum((np.transpose(wk) * np.transpose(spower)), 0) / np.sum(wk, 1)
+            spower_iter = spower_iter[:, np.newaxis]
 
-    return mt_spectrum
+        mt_spectrum = np.squeeze(spower_iter)
+
+    else:
+        # eigenvalue or uniform weights
+        mt_spectrum = np.dot(spower, wt)
+        mt_spectrum = np.reshape(mt_spectrum, nfft)  # reshape to 1D
+
+    return mt_spectrum[freq_inds]
