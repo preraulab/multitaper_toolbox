fdbb8d0d663e12c7b0d053c85658f68894c7c054
Thomas Possidente <tposside@gmail.com>
2021-03-01 10:41:41 -0500

    Update multitaper_spectrogram.m

--- matlab/multitaper_spectrogram.m
+++ matlab/multitaper_spectrogram.m
@@ -23,24 +23,25 @@ function [mt_spectrogram,stimes,sfreqs] = multitaper_spectrogram(varargin)
 %        sfreqs: 1xF vector of frequency bins for the spectrogram
 %  
 %   Example:
-%      Fs=200; %Sampling Frequency
-%      frequency_range=[0 25]; %Limit frequencies from 0 to 25 Hz
-%      taper_params=[3 5]; %Time bandwidth and number of tapers
-%      window_params=[4 1]; %Window size is 4s with step size of 1s
-%      min_nfft=0; %No minimum nfft
-%      detrend_opt='constant' %detrend each window by subtracting the average
-%      weighting='unity' %weight each taper at 1
-%      plot_on=True %plot spectrogram
-%      verbose=True %print extra info
+%   In this example we create some chirp data and run the multitaper spectrogram on it.
+%       Fs=200; %Sampling Frequency
+%       frequency_range=[0 25]; %Limit frequencies from 0 to 25 Hz
+%       taper_params=[3 5]; %Time bandwidth and number of tapers
+%       window_params=[4 1]; %Window size is 4s with step size of 1s
+%       min_nfft=0; %No minimum nfft
+%       detrend_opt='constant' %detrend each window by subtracting the average
+%       weighting='unity' %weight each taper at 1
+%       plot_on=true; %plot spectrogram
+%       verbose=true; %print extra info
 %
-%      %Generate sample chirp data
-%      t=1/Fs:1/Fs:600; %Create 10 minutes of data
-%      f_start=1;f_end=20; % Set chirp range in Hz
-%      data=chirp(t,f_start,t(end),f_end,'logarithmic');
+%       %Generate sample chirp data
+%       t=1/Fs:1/Fs:600; %Create 10 minutes of data
+%       f_start=1;f_end=20; % Set chirp range in Hz
+%       data=chirp(t,f_start,t(end),f_end,'logarithmic');
 %
-%      %Compute the multitaper spectrogram
-%      [spect,stimes,sfreqs] = multitaper_spectrogram(data,Fs,frequency_range, taper_params, window_params);
-%
+%       %Compute the multitaper spectrogram
+%       [spect,stimes,sfreqs] = multitaper_spectrogram(data,Fs,frequency_range, taper_params, window_params, min_nfft, detrend_opt, weighting, plot_on, verbose);
+%    
 %   This code is companion to the paper:
 %         "Sleep Neurophysiological Dynamics Through the Lens of Multitaper Spectral Analysis"
 %         Michael J. Prerau, Ritchie E. Brown, Matt T. Bianchi, Jeffrey M. Ellenbogen, Patrick L. Purdon
